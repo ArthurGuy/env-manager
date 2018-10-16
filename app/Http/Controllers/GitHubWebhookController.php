201e33a3578d7622115afa4be460cd28cb595a34
Arthur Guy <arthur@assys.net>
2018-10-16 18:56:57 +0100

    Debug

--- app/Http/Controllers/GitHubWebhookController.php
+++ app/Http/Controllers/GitHubWebhookController.php
@@ -21,11 +21,13 @@ class GitHubWebhookController extends Controller
         $logged = false;
         $payload = json_decode($request->getContent(), true);
         if (isset($payload['pull_request']) && $payload['action'] == 'closed' && $payload['pull_request']['merged']) {
+            $prNumber = $payload['number'];
+
             // PR has just been merged
-            Rollbar::log(Level::info(), 'Github PR has been merged', $payload);
+            Rollbar::log(Level::info(), 'Github PR has been merged #' . $prNumber, $payload);
             $logged = true;
 
-            $prNumber = $payload['number'];
+
             $reviews = $this->github->pullRequest()->reviews()->all(env('REVIEW_GITHUB_ORG'), env('REVIEW_GITHUB_REPO'), $prNumber);
             Rollbar::log(Level::info(), 'Github PR reviews', $reviews);
         }
